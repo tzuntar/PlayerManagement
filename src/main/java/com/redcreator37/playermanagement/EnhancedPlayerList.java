0652de3cac3b1007a25e6c99cf5fc42c20be6ec7
RedCreator37 <tobija.zuntar@gmail.com>
2021-01-03 18:33:04 +0100

    Split the replacePlaceholders method (renamed to fillDataFields) into two methods (one that calls related methods and another which replaces the placeholders and nothing more)

--- src/main/java/com/redcreator37/playermanagement/EnhancedPlayerList.java
+++ src/main/java/com/redcreator37/playermanagement/EnhancedPlayerList.java
@@ -40,61 +40,77 @@ public class EnhancedPlayerList implements Listener {
             entry[0] = PlayerManagement.prefs.adminPlayerEntry;
 
         // replace the placeholders and set the name, header and footer
-        p.setPlayerListName(replacePlaceholders(p, entry, Objects
+        p.setPlayerListName(fillDataFields(p, entry, Objects
                 .requireNonNull(PlayerManagement.ess).getUser(p).isAfk()));
-        p.setPlayerListHeaderFooter(replacePlaceholders(p,
+        p.setPlayerListHeaderFooter(fillDataFields(p,
                 PlayerManagement.prefs.playerListHeader, false),
-                replacePlaceholders(p, PlayerManagement.prefs.playerListFooter, false));
+                fillDataFields(p, PlayerManagement.prefs.playerListFooter, false));
     }
 
     /**
-     * Replaces the <code>{placeholders}</code> with real values
+     * Fills the values of placeholder data fields in the player list
+     * output with actual values and formats the output
      *
-     * @param p         the player to get the data from
-     * @param str       the array of lines
-     * @param playerAfk uses different formatting if the player is AFK
+     * @param player the player to get the data from
+     * @param str    the array of lines
+     * @param isAfk  uses different formatting if the player is AFK
      * @return the string with data inserted
      */
-    private static String replacePlaceholders(Player p, String[] str, boolean playerAfk) {
+    private static String fillDataFields(Player player, String[] str, boolean isAfk) {
         String rank = PlayerManagement.prefs.genericPlayerLabel;
-        if (p.hasPermission("playerlist.member"))
+        if (player.hasPermission("playerlist.member"))
             rank = PlayerManagement.prefs.memberPlayerLabel;
-        if (p.hasPermission("playerlist.vip"))
+        if (player.hasPermission("playerlist.vip"))
             rank = PlayerManagement.prefs.vipPlayerLabel;
-        if (p.hasPermission("playerlist.admin"))
+        if (player.hasPermission("playerlist.admin"))
             rank = PlayerManagement.prefs.adminPlayerLabel;
 
         StringBuilder builder = new StringBuilder();
         for (String s : str)
-            if (!s.matches("(.*)\\{(.*)}(.*)")) {
+            if (!s.matches("\\{.*}")) {
                 builder.append(s).append('\n');
             } else {
                 String company = "N/A";
                 if (s.contains("{playercompany}")) {    // for performance reasons
                     ServerPlayer target = PlayerManagement.players
-                            .get(p.getUniqueId().toString());
+                            .get(player.getUniqueId().toString());
                     if (target != null)
                         company = target.getCompany().getName();
                 }
-
-                builder.append(s.replace("{playername}", playerAfk
-                        ? "§o" + p.getName() + "§r" : p.getName())
-                        .replace("{playercount}", String.valueOf(p.getServer()
-                                .getOnlinePlayers().size()))
-                        .replace("{maxplayers}", String.valueOf(p.getServer()
-                                .getMaxPlayers()))
-                        .replace("{playerbalance}", PlayerRoutines.formatDecimal(BigDecimal
-                                .valueOf(PlayerManagement.eco.getBalance(p))))
-                        .replace("{playermail}", String.valueOf(Objects
-                                .requireNonNull(PlayerManagement.ess)
-                                .getUser(p).getMails().size()))
-                        .replace("{playerworld}", p.getWorld().getName())
-                        .replace("{playerrank}", rank)
-                        .replace("{playercompany}", company)
-                        .replace("{playergamemode}", p.getGameMode().toString().toLowerCase()))
-                        .append('\n');
+                builder.append(replacePlaceholders(s, player, company,
+                        rank, isAfk)).append('\n');
             }
         return builder.toString();
+    }
+
+    /**
+     * Replaces the {@code {placeholders}} with real values
+     *
+     * @param original the original string containing placeholders
+     * @param p        the player this instance of the list will be
+     *                 displayed to
+     * @param company  the player's company
+     * @param rank     the player's rank
+     * @param isAfk    whether the player is AFK or not
+     * @return the formatted string
+     */
+    private static String replacePlaceholders(String original, Player p, String company,
+                                              String rank, boolean isAfk) {
+        return original.replace("{playername}", isAfk
+                ? "§o" + p.getName() + "§r" : p.getName())
+                .replace("{playercount}", String.valueOf(p.getServer()
+                        .getOnlinePlayers().size()))
+                .replace("{maxplayers}", String.valueOf(p.getServer()
+                        .getMaxPlayers()))
+                .replace("{playerbalance}", PlayerRoutines.formatDecimal(BigDecimal
+                        .valueOf(PlayerManagement.eco.getBalance(p))))
+                .replace("{playermail}", String.valueOf(Objects
+                        .requireNonNull(PlayerManagement.ess)
+                        .getUser(p).getMails().size()))
+                .replace("{playerworld}", p.getWorld().getName())
+                .replace("{playerrank}", rank)
+                .replace("{playercompany}", company)
+                .replace("{playergamemode}", p.getGameMode().toString().toLowerCase());
     }
 
 }
